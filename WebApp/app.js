7bce879ac924daf232f72a7c63342e4f6278aa52
Atilla Bosma <atillabosma@hotmail.com>
2020-03-30 16:54:27 +0200

    - Installed passport-local and express-session for passport.js
    - Set up a secrret has for the json web token in the env file
    
    - Implemented basic set-up for authentication endpoint

--- WebApp/app.js
+++ WebApp/app.js
@@ -2,12 +2,14 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
 
 var indexRouter = require('./routes/index');
 var topicRouter = require('./routes/topics');
 var contentRouter = require('./routes/content');
+var authRouter = require('./routes/auth');
 
 // Loads environment variables from .env file
 require('dotenv').config();
@@ -18,6 +20,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(session({ secret: process.env.HASH_SECRET }))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('view engine', 'pug');
 
@@ -25,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/topics', topicRouter);
 app.use('/content', contentRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
