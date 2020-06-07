2b73730e2cc42e02d4be0442109e8a305b430fe9
Atilla Bosma <atillabosma@hotmail.com>
2020-06-07 14:14:31 +0200

    Database Connector key/cert fixing
    -- Database Connector cert loading now has a try catch. If the certs arent there it doesnt immediately crash now.

--- data/database_connector.js
+++ data/database_connector.js
@@ -4,8 +4,18 @@ const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const fs = require('fs');
 
-const key = fs.readFileSync(__dirname + '/../certs/server.key');
-const cert = fs.readFileSync(__dirname + '/../certs/server.cert')
+const key = null;
+const cert = null;
+
+try
+{
+    key = fs.readFileSync(__dirname + '/../certs/server.key');
+    cert = fs.readFileSync(__dirname + '/../certs/server.cert');
+}
+catch(error)
+{
+    return;
+}
 
 const db_connection_config_test =
 {
